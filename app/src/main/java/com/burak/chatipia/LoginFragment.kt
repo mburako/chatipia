f44f21262ff98a90be59adaa4de57176cee99482
burak <burakozturk.cs@gmail.com>
2021-04-08 12:47:48 +0300

    Navigation is done

---
+++ app/src/main/java/com/burak/chatipia/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.burak.chatipia
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loginButton.setOnClickListener {
+            NavHostFragment.findNavController(this).navigate(R.id.action_to_chat)
+        }
+    }
+}
