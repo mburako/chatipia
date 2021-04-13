b06d1434753435401c4b0f1f79d83081f3bf85b2
burak <burakozturk.cs@gmail.com>
2021-04-13 13:12:11 +0300

    Navigation start destination is done

--- app/src/main/java/com/burak/chatipia/ui/MainActivity.kt
+++ app/src/main/java/com/burak/chatipia/ui/MainActivity.kt
@@ -6,6 +6,8 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.burak.chatipia.R
 import com.burak.chatipia.data.local.SharedPrefUtils
 
@@ -18,6 +20,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setStartDestination()
+
         supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         supportActionBar!!.setDisplayShowCustomEnabled(true)
         supportActionBar!!.setCustomView(R.layout.custom_action_bar)
@@ -25,9 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         leaveButton = view.findViewById(R.id.leave_button)
         leaveButton.setOnClickListener {
-            SharedPrefUtils.resetLoggedInUserName(this)
-            setTitle(getString(R.string.app_name))
-            onBackPressed()
+           doLogout()
         }
 
         appTitle = view.findViewById(R.id.action_bar_title)
@@ -47,5 +49,36 @@ class MainActivity : AppCompatActivity() {
                leaveButton.visibility = View.VISIBLE
            }
         }
+    }
+
+    override fun onBackPressed() {
+        // Prevent back navigation
+        finish()
+    }
+
+    private fun setStartDestination() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        val navInflater = navController.navInflater
+        val graph = navInflater.inflate(R.navigation.main_navigation)
+
+        if (SharedPrefUtils.getLoggedInUserName(this).isNullOrEmpty()) {
+            graph.startDestination = R.id.loginFragment
+        } else {
+            graph.startDestination = R.id.chatFragment
+        }
+
+        navController.graph = graph
+    }
+
+    private fun doLogout() {
+        SharedPrefUtils.resetLoggedInUserName(this)
+        setTitle(getString(R.string.app_name))
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        navController.navigate(R.id.action_to_login)
     }
 }
